61fb4bbadc44521a18a8f72e7458013b830f93e7
LE0N4RDOR4M0S <leomatisa1000@gmail.com>
2025-09-09 19:22:56 -0400

    feat: novos inserts

--- sql/sopro_db.sql
+++ sql/sopro_db.sql
@@ -28,6 +28,6 @@ INSERT INTO anomalias (designacao, apelido, classe_risco, descricao, procediment
 ('UF-666-IC', 'Zezinhol', 'Apollyon', 'O único acima dos 4 cavaleiros do apocalipse.', 'Permitir interacao e monitorar comportamento, em caso de emergência procurar matéria de verão.', 'ze.PNG', 3),
 ('EA-074-BR', 'A Estátua', 'Euclídeo', 'Uma estatua animada que se move rapidamente quando não é observada.', 'Deve ser mantida em uma cela trancada e observada por pelo menos dois funcionarios.', 'scp173.jpg', 1),
 ('TE-076-BR', 'A Maquina', 'Seguro', 'Um dispositivo mecanico capaz de refinar objetos.', 'Acesso restrito apenas a pessoal autorizado.', 'scp914.png', 3),
-('SO-077-BR', 'O Velho', 'Keter', 'Uma entidade que pode viver para sempre.', 'Caso haja atitude hostis, conversar e elogiar os desenhos de sua sobrinha.', 'scp106.jpg', 2),
-('BI-078-BR', 'Os gêmeos', 'Euclídeo', 'Dois irmãos idênticos que compartilham pensamentos e sensações, capazes de qualquer coisa quando juntos.', 'Separar os gêmeos caso apresentem comportamento hostil.', 'scp049.jpg', 1),
+('SO-077-BR', 'O Velho', 'Keter', 'Uma entidade que pode viver para sempre.', 'Caso haja atitude hostis, conversar e elogiar os desenhos de sua sobrinha.', 'scp106.jpeg', 2),
+('BI-078-BR', 'Os gêmeos', 'Euclídeo', 'Dois irmãos idênticos que compartilham pensamentos e sensações, capazes de qualquer coisa quando juntos.', 'Separar os gêmeos caso apresentem comportamento hostil.', 'scp049.jpeg', 1),
 ('IN-080-BR', 'O Computador Antigo', 'Euclídeo', 'Um computador com inteligencia artificial hostil.', 'Manter desconectado e em uma cela segura.', 'scp079.png', 2);
