f8b75b1807e31cbe68ff203d886a33584e0761d2
Leonardo Ramos <leomatisa1000@gmai.com>
2025-09-09 08:42:45 -0400

    chore: ajuste de configurações do host interno

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
+<?php
 // Autor: Leonardo de Oliveira Ramos
 // Desenvolvido em 09/2025
 // Avaliação prática da disciplina de Tópicos Especiais em Desenvolvimento de Sistemas I
-<?php
 require_once 'includes/config.php';
 require_once 'includes/funcoes.php';
 ?>
